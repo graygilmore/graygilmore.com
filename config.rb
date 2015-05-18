c673bcd4c199d43a361e9423b0d3a5e952193a6e
Gray Gilmore <graygilmore@gmail.com>
2015-05-18 15:01:02 -0700

    Activate s3_sync

--- config.rb
+++ config.rb
@@ -3,6 +3,11 @@ set :css_dir, "stylesheets"
 set :js_dir, "javascripts"
 set :images_dir, "images"
 
+activate :s3_sync do |s3_sync|
+  s3_sync.bucket = "www.graygilmore.com"
+  s3_sync.region = "us-west-2"
+end
+
 configure :build do
 
 end
