0e6bbd52ea250ab5be4f39c1a852f79ee13d74c2
Gray Gilmore <graygilmore@gmail.com>
2015-12-22 13:57:07 -0500

    Remove middleman-s3_sync
    We're going to switch over to using Rsync.

--- config.rb
+++ config.rb
@@ -3,11 +3,6 @@ set :css_dir, "stylesheets"
 set :js_dir, "javascripts"
 set :images_dir, "images"
 
-activate :s3_sync do |s3_sync|
-  s3_sync.bucket = "www.graygilmore.com"
-  s3_sync.region = "us-west-2"
-end
-
 configure :build do
   activate :minify_css
 end
