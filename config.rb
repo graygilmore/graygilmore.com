277cd902282563b19f2b79f6d130c23cd93ec5c6
Gray Gilmore <graygilmore@gmail.com>
2015-12-22 15:03:35 -0500

    Use rsync to deploy to Charmander

--- config.rb
+++ config.rb
@@ -3,6 +3,12 @@ set :css_dir, "stylesheets"
 set :js_dir, "javascripts"
 set :images_dir, "images"
 
+activate :deploy do |deploy|
+  deploy.method = :rsync
+  deploy.host = "charmander"
+  deploy.path = "/var/www/graygilmore.com/html"
+end
+
 configure :build do
   activate :minify_css
 end
