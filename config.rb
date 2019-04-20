5d08f79d07f8a7f81b0d75e51e053ca85fe1b818
Gray Gilmore <graygilmore@gmail.com>
2019-04-19 18:22:20 -0700

    Add travel recommendations for Montreal

--- config.rb
+++ config.rb
@@ -1,3 +1,7 @@
+data.travel.cities.each do |city|
+  proxy "/travel/#{city[0]}/index.html", "/city.html", :locals => { :city => city[1] }, :ignore => true
+end
+
 activate :deploy do |deploy|
   deploy.deploy_method = :rsync
   deploy.host = "charmander"
@@ -7,4 +11,10 @@ end
 configure :build do
   activate :minify_css
   activate :asset_hash
+end
+
+helpers do
+  def markdown(text)
+    Tilt['markdown'].new(context: @app) { text }.render
+  end
 end
